7f2acb4cca6f8f27fe5c56f32f209424f8fb323c
local <local@dummy.co.jp>
2015-03-16 13:49:00 +0900

    METAタグ取得の追加とソースファイルを外部化

--- output-siteinfo.js
+++ output-siteinfo.js
@@ -1,31 +1,26 @@
 /********************************************
  * 指定したページの特定の情報をcsvに出力する
  ********************************************/
-var fs = require('fs');
+var fs   = require('fs');
+var json = require('./output_file/src.json');  // URLが配列形式で記載されたファイルを読み込む
 
-/* ページURLを設定 */
-var LINKS = [
-    'http://www.yahoo.co.jp/',
-    'http://www.google.co.jp/',
-    'http://www.msn.com/'
-    // Basic認証を直接含めてもOK http://userName:password@example.jp
-];
 
 /* データを格納する多次元配列を作成する */
-/* 2番目の配列には以下を格納 */
+/* 2番目の配列には以下を格納するための '' 空スペースを用意 */
 /*
- data[i][0] = url
- data[i][1] = title
+  data[i][0] = url
+  data[i][1] = title
+  data[i][2] = meta: description
+  data[i][3] = meta: keyword
 */
 var data = (function() {
     var arr = [];
-    var i = 0;
-    var j = 0;
-    var l = LINKS.length;
+    var i, j;
+    var l = json.length;
 
-    for ( i = 0; i < l; i++) {
+    for (i = 0; i < l; i++) {
         arr[i] = [];
-        for( j = 0; j < 3; j++) {
+        for(j = 0; j < 4; j++) {
             arr[i][j] = '';
         }
     }
@@ -33,22 +28,50 @@ var data = (function() {
 })();
 
 
-/* casper処理 */
-var casper = require('casper').create();
-casper.start().each(LINKS, function(self, link, i) {
+/* casperの定義 */
+/* ローカルにあるjQueryを実行できるように */
+var casper = require('casper').create({clientScripts: ['jquery-1.11.2.min.js']});
 
-    data[i][0] = link;
+/** 
+ * casperのループ実行
+ * @param {array}  json: URLを設定した配列
+ * @param {object} self: Casperオブジェクト
+ * @param {stings} link: 処理している現在のURL
+ */
+casper.start().each(json, function(self, link, i) {
 
     /* 連続実行 */
     self
         /* 指定urlを開く */
         .thenOpen(link, function () {
-            data[i][1] = this.getTitle() || '';
+
+            /* URLとタイトルの取得 */
+            var title = this.getTitle();
+            var url   = this.getCurrentUrl();
+
+            /* metaタグの取得 */
+            var metaDes = this.evaluate(function() {
+                return $('meta[name=description]').attr('content');
+            });
+            var metaKey = this.evaluate(function() {
+                return $('meta[name=keywords]').attr('content');
+            });
+
+            /* dataの2番目の各配列にデータを追加 */
+            /* カンマ区切りが入ってきても大丈夫なように各項目には "" でエスケープする */
+            /* データが存在しない時（null）は "" は付けない */
+            data[i][0] = '"' + url + '"';
+            data[i][1] = (title   !== null) ? '"' + title   + '"' : '';
+            data[i][2] = (metaDes !== null) ? '"' + metaDes + '"' : '';
+            data[i][3] = (metaKey !== null) ? '"' + metaKey + '"' : '';
+
+            this.echo('Loading... ' + title);
         })
 
-        /* 読み込んだら */
+        /* 読み込んだらcsvファイルに書き込む */
         .then(function() {
-            fs.write('output.csv', data.join('\n'));
+            /* ファイルはutf-8なので、必要に応じてツールで文字コード変換を行う */
+            fs.write('./output_file/output.csv', data.join('\n'));
         })
     ;
 });
